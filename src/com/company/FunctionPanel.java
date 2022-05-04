668dfcb8f54207ed713303f1c1c8e4524a3c7195
Huynh Anh Kiet <huynhanhkiet179@gmail.com>
2022-05-04 10:45:45 +0700

    play and stop button

--- src/com/company/FunctionPanel.java
+++ src/com/company/FunctionPanel.java
@@ -1,18 +1,32 @@
 package com.company;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 public class FunctionPanel extends JPanel {
 
     private JLabel label;
+    private JTextField textField;
+    private JButton generateButton;
+    private JButton deleteButton;
 
     private void initialize() {
         label = new JLabel("Số phần tử mảng");
+        textField = new JTextField();
+        textField.setColumns(10);
+
+        generateButton = new JButton("Tạo mảng");
+        deleteButton = new JButton("Xóa mảng");
+
         add(label);
+        add(textField);
+        add(generateButton);
+        add(deleteButton);
     }
 
     public FunctionPanel(JFrame frame) {
--- src/com/company/VisualizerPanel.java
+++ src/com/company/VisualizerPanel.java
@@ -1,14 +1,47 @@
 package com.company;
 
+import java.awt.Image;
+
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
 public class VisualizerPanel extends JPanel {
 
+    private JButton playButton;
+    private JButton stopButton;
+
+    private Icon playIcon;
+    private Icon stopIcon;
+
+    private void getIcon() {
+        playIcon = new ImageIcon(
+                new ImageIcon(getClass().getResource("./images/play-solid.png"))
+                        .getImage().getScaledInstance(10, 12,
+                                Image.SCALE_AREA_AVERAGING));
+
+        stopIcon = new ImageIcon(new ImageIcon(getClass()
+                .getResource("./images/stop-solid.png")).getImage()
+                .getScaledInstance(10, 12, Image.SCALE_SMOOTH));
+    }
+
+    private void initialize() {
+        getIcon();
+        playButton = new JButton(playIcon);
+        stopButton = new JButton(stopIcon);
+
+        add(playButton);
+        add(stopButton);
+    }
+
     public VisualizerPanel(JFrame frame) {
         super();
+
+        initialize();
 
         TitledBorder border = BorderFactory.createTitledBorder("Minh họa thuật toán");
         border.setTitleFont(FontManager.titleFont);
