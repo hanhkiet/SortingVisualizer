476aedf52bcb4e37640a2bbb190b255452f8264b
DatN <71223440+DatNguyen-267@users.noreply.github.com>
2022-05-10 14:35:32 +0700

    commit complete all

--- src/com/company/codeCpn/BBSort.java
+++ src/com/company/codeCpn/BBSort.java
@@ -2,6 +2,9 @@ package com.company.codeCpn;
 
 import javax.swing.*;
 import com.company.components.MyJList;
+import com.company.model.BubbleSortValue;
+import com.company.model.SortValue;
+
 import java.awt.*;
 
 public class BBSort extends Sort {
@@ -11,10 +14,12 @@ public class BBSort extends Sort {
       "       if (arr[j] > arr[j + 1])",
       "           swap(arr[j], arr[j + 1]);",
       " " };
-  private int indexKeySwap = 3;
+  private int indexKeySwap = 4;
   private JLabel label;
   private int i = -1;
   private int j = -1;
+  private String type;
+  private BubbleSortValue currentValue;
 
   public BBSort() {
     super();
@@ -35,8 +40,9 @@ public class BBSort extends Sort {
 
     this.setBackground(Color.WHITE);
     this.jList = new MyJList(lsCode);
-    this.add(label, BorderLayout.NORTH);
+    // this.add(label, BorderLayout.NORTH);
     this.add(jList, BorderLayout.CENTER);
+    this.currentValue = new BubbleSortValue(-999, -999);
   }
 
   public void init() {
@@ -48,9 +54,10 @@ public class BBSort extends Sort {
 
   @Override
   public void next() {
+    this.type = "NONE";
     if (arr.length > 0 && this.isSuccess == false) {
       this.setIndex(index + 1);
-      if (this.index > 3) {
+      if (this.index > 4) {
         this.setIndex(1);
       }
       if (this.index == 0) {
@@ -59,11 +66,10 @@ public class BBSort extends Sort {
       if (this.index == 1) {
         if (j == -1) {
           this.setI(i + 1);
+          this.type = "LOAD_DATA";
           if (this.i == arr.length - 1) {
             this.isSuccess = true;
-            this.label.setText(arr[0] + " - " + arr[1] +
-                " - " + arr[2] + " - " + arr[3] +
-                " - " + arr[4] + " - " + arr[5] + " isS: " + isSuccess);
+            this.type = "SORT_SUCCESS";
             return;
           }
         }
@@ -74,28 +80,46 @@ public class BBSort extends Sort {
           this.setIndex(0);
           this.setJ(-1);
         }
-
+        this.type = "LOAD_DATA";
       }
-      if (this.index == this.indexKeySwap) {
+      if (this.index == 4) {
         if (arr[j] > arr[j + 1]) {
           this.setIsSwap(true);
           int temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
-          this.label.setText(arr[0] + " - " + arr[1] +
-              " - " + arr[2] + " - " + arr[3] +
-              " - " + arr[4] + " - " + arr[5] + " isS: " + isSuccess);
-        }
-
-        else {
+          this.type = "SWAP";
+        } else {
           this.setIsSwap(false);
           this.setIndex(1);
         }
 
       } else
         this.setIsSwap(false);
+      this.setValueCallBack();
+      // this.print();
       this.jList.setSelectedIndex(index);
     }
+  }
+
+  public SortValue getValue() {
+    return this.values;
+  }
+
+  private void setValueCallBack() {
+    this.values = new SortValue();
+
+    this.currentValue.setI(this.i);
+    this.currentValue.setJ(this.j);
+    this.values = currentValue;
+    this.values.setNameSort("Bubble sort");
+    this.values.setTypeAction(this.type);
+  }
+
+  public void print() {
+    this.label.setText(arr[0] + " - " + arr[1] +
+        " - " + arr[2] + " - " + arr[3] +
+        " - " + arr[4] + " - " + arr[5] + " isS: " + isSuccess);
   }
 
   public int getIndex() {
